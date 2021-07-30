788db0bbc824b7e5e2fd950124643460695af98e
pebble-fish <pebble.fishie@gmail.com>
2021-07-30 00:15:15 -0400

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'my-app';
+
+  columnDefs = [
+    { field: 'course', sortable: true, filter: true, editable: true },
+    { field: 'room', sortable: true, filter: true, editable: true },
+    { field: 'block1', sortable: true, filter: true, editable: true },
+    { field: 'block2', sortable: true, filter: true, editable: true },
+    { field: 'block3', sortable: true, filter: true, editable: true },
+    { field: 'block4', sortable: true, filter: true, editable: true },
+    { field: 'block5', sortable: true, filter: true, editable: true },
+    { field: 'block6', sortable: true, filter: true, editable: true }
+];
+
+  rowData = [
+      { course: 'Toyota', room: 'Celica', block1: 'A1', block2: 'A2', block3:'A3', block4: 'A4', block5: 'B1', block6: 'B2' },
+      { course: 'Toyota', room: 'Celica', block1: 'A1', block2: 'A2', block3:'A3', block4: 'A4', block5: 'B1', block6: 'B2' },
+      { course: 'Toyota', room: 'Celica', block1: 'A1', block2: 'A2', block3:'A3', block4: 'A4', block5: 'B1', block6: 'B2' },
+      { course: 'Toyota', room: 'Celica', block1: 'A1', block2: 'A2', block3:'A3', block4: 'A4', block5: 'B1', block6: 'B2' },
+      { course: 'Toyota', room: 'Celica', block1: 'A1', block2: 'A2', block3:'A3', block4: 'A4', block5: 'B1', block6: 'B2' },
+      { course: 'Toyota', room: 'Celica', block1: 'A1', block2: 'A2', block3:'A3', block4: 'A4', block5: 'B1', block6: 'B2' },
+      { course: 'Toyota', room: 'Celica', block1: 'A1', block2: 'A2', block3:'A3', block4: 'A4', block5: 'B1', block6: 'B2' },
+      { course: 'Toyota', room: 'Celica', block1: 'A1', block2: 'A2', block3:'A3', block4: 'A4', block5: 'B1', block6: 'B2' },
+    ];
+  title: any;
 }
