99245326004420886a9296b4da2bd66b213f6cc6
pebble-fish <pebble.fishie@gmail.com>
2021-08-01 23:01:17 -0400

    import functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,15 @@ import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
 })
 
 export class AppComponent implements OnInit{
-  @ViewChild('agGrid') agGrid: AgGridAngular | undefined;
+  
+@ViewChild('agGrid') agGrid!: AgGridAngular;
+  
   private gridApi: any;
   private gridColumnApi: any;
   private defaultColDefs: any;
   public modules: Module[] = AllCommunityModules;
 
-rowData: Observable<any[]> | undefined;
+rowData!: Observable<any[]>;
 title: any;
 
 defaultColDef = {
@@ -46,5 +48,68 @@ columnDefs = [
   ngOnInit(): void { 
     this.rowData = this.http.get<any[]>('/assets/mock-courses.json');
   };
+
+
+getSelectedRows(): void {
+  const selectedNodes = this.agGrid.api.getSelectedNodes();
+  const selectedData = selectedNodes.map(node => node.data);
+  const selectedDataStringPresentation = selectedData.map(node => `${node.course} ${node.room} ${node.I} ${node.II} ${node.III} ${node.IV} ${node.V} ${node.VI} `).join(', ');
+
+  alert(`Selected nodes: ${selectedDataStringPresentation}`);
+}
+
+getAllRows():void{
+  //as no paging setup, so the diaply row is the same as after selecting all
+  let iDispayRowCount = this.agGrid.api.getDisplayedRowCount();
+  alert(`iDispayRowCount: ${iDispayRowCount}\n `);
+
+  // iterate through every node in the grid -> use each node's col to get info.  example: 'make' from the car row
+  //you can see the result from console
+  let iCount = 0;
+  this.agGrid.api.forEachNode((rowNode, index) => {
+    iCount++;
+  console.log('node ' + rowNode.data.make +','+ rowNode.data.model + ',' + rowNode.data.price + ' is in the grid');
+});
+  
+console.log('We are in here ' + iCount);
+}
+
+//add a new row and resize the table
+deleteSelectedRows(): void
+{
+  const selectedNodes = this.agGrid.api.getSelectedNodes();
+  let iCount = selectedNodes.length;
+  this.agGrid.api.removeItems(selectedNodes);
+
+  alert(`RemoveItems: ${iCount} `);
+}
+
+//add a new row and resize the table
+addARow() : void
+{
+  const carMake = "Tesla";
+  const carModel = "Mode S";
+  const carPrice = "56000";
+  const car = {make: carMake, model: carModel, price: carPrice};
+  this.agGrid.api.addItems([car]);
+
+}
+//add a new row and resize the table
+addAEmptyRow() : void
+{
+  const carMake = "";
+  const carModel = "";
+  const carPrice = "";
+  const emptyCar = {make: carMake, model: carModel, price: carPrice};
+  this.agGrid.api.addItems([emptyCar]);
+
+}
+
+selectAllRows():void{
+  this.agGrid.api.selectAll();
+}
+deselectAllRows():void{
+  this.agGrid.api.deselectAll();
+}
 
 }
